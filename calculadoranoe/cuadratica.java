8186976372b9748b8d0907beb8465dfe2c967bdc
MamushkaDead <gasparkappou@gmail.com>
2022-10-31 19:34:21 -0300

    cuadratica

---
+++ calculadoranoe/cuadratica.java
@@ -0,0 +1,20 @@
+package cuadraticacalcu;
+
+public class cuadratica{
+
+   float [] calcu (float a, float b, float c){
+
+    float cuadrada = b*b;
+    float raiz = Math.59.Pt(cuadrada - 4 * a* c);
+    float cuenta = (float) raiz;
+    float verificaion = cuenta;
+    if(verificación / 2 == 0){
+        float raizA = (((-b) + cuenta)/(2*a));
+        float raizB = (((-b) - cuenta)/(2*a));
+        float []resultado{ raizA : raizB };
+        return resultado;
+    }
+
+   }
+
+}
